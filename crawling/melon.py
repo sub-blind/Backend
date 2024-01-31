89d6ada5cb9efcfc6d2d28796de58fc96f2f0351
sub-blind <rlawotjq206@gmail.com>
2024-01-31 10:40:57 +0900

    melon

--- crawling/melon.py
+++ crawling/melon.py
@@ -1,30 +1,32 @@
+import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-import time
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-time.sleep(2)
+header_user = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
+}
 
-url = "http://www.melon.com/chart/"
-driver.get(url)
-time.sleep(5)
+base_url = "https://www.melon.com/chart/month/index.htm"
 
-html_str = driver.page_source
+req = requests.get(base_url, headers=header_user)
 
-soup = BeautifulSoup(html_str, "html.parser")
+html = req.text
+soup = BeautifulSoup(html, "html.parser")
 
-# 차트 항목을 가져와서 출력합니다.
-chart_items = soup.select(".lst50")
+# num = 1 << 곡 순위 할 수 있는 다른 방법
+lst_all = soup.find_all(class_=["lst50", "lst100"])
 
-print("Melon 차트 순위:")
-for item in chart_items:
-    # strip()은 공백을 제거해줍니다.
-    rank = item.select_one(".rank").text.strip()
-    title = item.select_one(".ellipsis.rank01").text.strip()
-    artist = item.select_one(".checkEllipsis").text.strip()
-    print(f"{rank}위 제목:{title}  가수:{artist}")
+for i in lst_all:
+    up = i.select_one(".up")
 
+    if up:
+        title = i.select_one(".ellipsis.rank01 a")
+        name = i.select_one(".ellipsis.rank02 a")
+        rank = i.select_one(".wrap.t_center .rank")
+        album = i.select_one(".ellipsis.rank03 a")
 
-driver.quit()
+        print(f"순위: {rank.text}위")
+        print(f"전 달 대비: {up.text}순위 상승")
+        print(f"제목: {title.text}")
+        print(f"가수: {name.text}")
+        print(f"앨범: {album.text}")
+        print()
