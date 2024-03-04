88feae5d09ac6e6f97f4e9e1e1936a01c6a7dc33
sub-blind <rlawotjq206@gmail.com>
2024-03-05 01:22:39 +0900

    Practice_blog_0305

--- Flask/Part3/blog_practice/app.py
+++ Flask/Part3/blog_practice/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_mysqldb import MySQL
-import yaml  # pip install pyyaml
+import yaml
 from flask_smorest import Api
 from posts_routes import create_posts_blueprint
 
@@ -12,20 +12,26 @@ app.config["MYSQL_USER"] = db["mysql_user"]
 app.config["MYSQL_PASSWORD"] = db["mysql_password"]
 app.config["MYSQL_DB"] = db["mysql_db"]
 
-
 mysql = MySQL(app)
 
-# blueprint 설정 및 등록
-app.config["API_TITLE"] = "My API"
+# blueprint 설정
+app.config["API_TITLE"] = "Blog API List"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.1.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+app.config["OPENAPI_SWAGGER_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 api = Api(app)
 posts_blp = create_posts_blueprint(mysql)
 api.register_blueprint(posts_blp)
+
+from flask import render_template
+
+
+@app.route("/blogs")
+def manage_blogs():
+    return render_template("posts.html")
 
 
 if __name__ == "__main__":
--- Flask/Part3/blog_practice/posts_routes.py
+++ Flask/Part3/blog_practice/posts_routes.py
@@ -4,10 +4,7 @@ from flask_smorest import Blueprint, abort
 
 def create_posts_blueprint(mysql):
     posts_blp = Blueprint(
-        "posts",
-        __name__,
-        description="posts api",
-        url_prefix="/posts",
+        "posts", __name__, description="posts api", url_prefix="/posts"
     )
 
     @posts_blp.route("/", methods=["GET", "POST"])
@@ -25,22 +22,16 @@ def create_posts_blueprint(mysql):
             post_list = []
 
             for post in posts:
-                post_list.append(
-                    {
-                        "id": post[0],
-                        "title": post[1],
-                        "content": post[2],
-                    }
-                )
+                post_list.append({"id": post[0], "title": post[1], "content": post[2]})
             return jsonify(post_list)
 
         # 게시글 생성
-        elif request.method == "POST":
+        if request.method == "POST":
             title = request.json.get("title")
             content = request.json.get("content")
 
             if not title or not content:
-                abort(400, message="title 또는 content가 없습니다.")
+                abort(400, message="Title or Content cannot be empty")
 
             sql = "INSERT INTO posts(title, content) VALUES(%s, %s)"
             cursor.execute(sql, (title, content))
@@ -48,57 +39,50 @@ def create_posts_blueprint(mysql):
 
             return jsonify({"message": "success"}), 201
 
-    # 게시글 상세 조회
-    # 게시글 수정 및 삭제
     @posts_blp.route("/<int:id>", methods=["GET", "PUT", "DELETE"])
     def post(id):
         cursor = mysql.connection.cursor()
 
         if request.method == "GET":
-            sql = f"SELECT * FROM posts WHERE id={id}"
+            sql = f"SELECT * FROM posts WHERE id = {id}"
             cursor.execute(sql)
-            post = cursor.fetchone()
+            post = cursor.fetchall()
 
             if not post:
-                abort(404, message="해당 게시글이 없습니다.")
-            return {
-                "id": post[0],
-                "title": post[1],
-                "content": post[2],
-            }
+                abort(404, "Not found post")
+            return {"id": post[0], "title": post[1], "content": post[2]}
 
         elif request.method == "PUT":
             title = request.json.get("title")
             content = request.json.get("content")
 
             if not title or not content:
-                abort(400, message="title 또는 content가 없습니다.")
+                abort(400, message="Not found title, content")
 
-            sql = "SELECT * FROM posts WHERE id=%s"
+            sql = "SELETE * FROM posts WHERE id=%s"
             cursor.execute(sql, (id,))
             post = cursor.fetchone()
 
             if not post:
-                abort(404, message="해당 게시글이 없습니다.")
+                abort(404, "Not found post")
 
             sql = "UPDATE posts SET title=%s, content=%s WHERE id=%s"
             cursor.execute(sql, (title, content, id))
             mysql.connection.commit()
 
-            return jsonify({"message": "Successfully updated title & content"})
-
+            return jsonify({"msg": "Successfully updated title & content"})
         elif request.method == "DELETE":
             sql = "SELECT * FROM posts WHERE id=%s"
             cursor.execute(sql, (id,))
             post = cursor.fetchone()
 
             if not post:
-                abort(404, message="해당 게시글이 없습니다.")
+                abort(404, "Not found post")
 
             sql = "DELETE FROM posts WHERE id=%s"
             cursor.execute(sql, (id,))
             mysql.connection.commit()
 
-            return jsonify({"message": "Successfully deleted post"})
+            return jsonify({"msg": "Successfully deleted id"})
 
     return posts_blp
