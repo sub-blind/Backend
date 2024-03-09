46a3a878287c57cc64d0ceead37ad1ba5304dd5a
sub-blind <rlawotjq206@gmail.com>
2024-03-10 05:55:07 +0900

    until_custom-admin

---
+++ Django/Django_2/boards/models.py
@@ -0,0 +1,15 @@
+# models.py
+from django.db import models
+from common.models import CommonModel
+
+
+class Board(CommonModel):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    writer = models.CharField(max_length=30)
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+    reviews = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
